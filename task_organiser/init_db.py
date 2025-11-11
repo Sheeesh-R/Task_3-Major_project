c9018f1160409395ac419ac9d14566ca3821a4fa
shreeesh r <shrishray15@gmail.com>
2025-11-11 21:39:26 +1100

    Allowed for easier editing and deleting of task.

--- task_organiser/init_db.py
+++
@@ -1,7 +0,0 @@
-from rjsandbox1.app import create_app
-from rjsandbox1.db import init_db, get_db
-
-app = create_app()
-with app.app_context():
-    init_db()
-    print("Database initialized successfully!")
